941716b59e78379f3aeb898db18b92ab7e396813
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-01-27 21:53:42 +0330

    init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <main
+    font-sans
+    class="flex flex-col h-screen justify-center items-center"
+    un-bg="gray-50 dark:gray-900"
+    un-text="gray-700 dark:gray-200"
+  >
+    <h1 class="mb-3 text-cool-gray-600 dark:text-cool-gray-400 text-xl">SIMPLEDSHEET</h1>
+    <Footer />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'Simpledsheet',
+  meta: [
+    {
+      name: `description`,
+      content: `A simple implementation of the standard spreadsheet.`,
+    },
+  ],
+})
+</script>
