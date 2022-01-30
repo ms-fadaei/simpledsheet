c4167ad85e24d233b7bf4b435963dd1671f0b0af
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-01-30 10:07:31 +0330

    feat: support keyboard navigations

--- src/components/SimpledSheet.vue
+++ src/components/SimpledSheet.vue
@@ -13,7 +13,13 @@
         <tr v-for="row in rows" :key="row">
           <th un-border="1" class="w-24 h-10">{{ row }}</th>
           <td v-for="col in cols" :key="col" un-border="1">
-            <SimpledSheetCell :row="row" :col="col" />
+            <SimpledSheetCell
+              :row="row"
+              :col="col"
+              :is-active="row === currentRow + 1 && col === currentCol + 1"
+              @select="setCurrentCell"
+              @move="moveCell"
+            />
           </td>
         </tr>
       </tbody>
@@ -29,6 +35,23 @@ const $props = defineProps<{
   cols: number
 }>()
 
+// set up the initial state
 const { rows, cols } = toRefs($props)
+const currentRow = ref(-1)
+const currentCol = ref(-1)
+
+// init table cells
 initCells(rows.value, cols.value)
+
+// set current cell position when it is selected
+function setCurrentCell({ row, col }: { row: number; col: number }) {
+  currentRow.value = row - 1
+  currentCol.value = col - 1
+}
+
+// move current cell position
+function moveCell({ rowShift, colShift }: { rowShift: number; colShift: number }) {
+  currentRow.value = (currentRow.value + rowShift + rows.value) % rows.value
+  currentCol.value = (currentCol.value + colShift + cols.value) % cols.value
+}
 </script>
--- src/components/SimpledSheetCell.vue
+++ src/components/SimpledSheetCell.vue
@@ -6,7 +6,7 @@
     @vnode-mounted="onInputMounted"
     @change="updateCell"
     @blur="isEditMode = false"
-    @keydown="(e) => e.keyCode === 13 && (isEditMode = false)"
+    @keydown="onKeyDown"
   />
   <span v-else class="flex items-center p-1 w-24 h-10 cursor-text" @click="isEditMode = true">
     {{ useCellValue(row, col) }}
@@ -19,19 +19,65 @@ import { useCellValue, useCells } from '~/composables'
 const $props = defineProps<{
   row: number
   col: number
+  isActive: boolean
 }>()
 
-const { row, col } = toRefs($props)
-
+// set up the initial state
+const $emit = defineEmits(['select', 'move'])
+const { row, col, isActive } = toRefs($props)
 const isEditMode = ref(false)
+
+// get cells 2D array
 const cells = useCells()
 
+// set focus when cell input is mounted
 function onInputMounted({ el }: { el: HTMLInputElement }) {
   el.focus()
 }
 
+// emit select event when cell is on edit mode
+watch(isEditMode, (isEditMode) => {
+  if (isEditMode) {
+    $emit('select', { row: row.value, col: col.value })
+  }
+})
+
+// auto editMode when cell is activated from parent
+watch(isActive, (isActive) => {
+  if (isActive) {
+    isEditMode.value = true
+  }
+})
+
+// emit move event when user is using keys
+const mappingKeys: {
+  [key: string]: { rowShift: number; colShift: number }
+} = {
+  Tab: { rowShift: 0, colShift: 1 },
+  Enter: { rowShift: 1, colShift: 0 },
+  ArrowUp: { rowShift: -1, colShift: 0 },
+  ArrowDown: { rowShift: 1, colShift: 0 },
+  ArrowLeft: { rowShift: 0, colShift: -1 },
+  ArrowRight: { rowShift: 0, colShift: 1 },
+}
+
+function onKeyDown(e: KeyboardEvent) {
+  if (mappingKeys[e.key]) {
+    e.preventDefault()
+    isEditMode.value = false
+
+    if (e.shiftKey && !e.key.includes('Arrow')) {
+      mappingKeys[e.key].rowShift *= -1
+      mappingKeys[e.key].colShift *= -1
+    }
+
+    $emit('move', mappingKeys[e.key])
+  }
+}
+
+// update cell value when input is changed
 function updateCell(e: Event) {
   const { value } = e.target as HTMLInputElement
-  cells.value[row.value - 1][col.value - 1] = value.toString().trim()
+  cells.value[row.value - 1][col.value - 1] = value.trim()
 }
 </script>
